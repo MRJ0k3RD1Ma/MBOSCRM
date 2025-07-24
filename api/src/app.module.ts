07f5fed3f876fc12c6906255138c6851f38ae479
Tanjiro017 <otabekotanazarov77@gmail.com>
2025-07-24 18:29:24 +0500

    create product group

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { UserModule } from './modules/user/user.module';
 import { SettingsModule } from './modules/settings/settings.module';
+import { ClientModule } from './modules/client/client.module';
 
 @Module({
   controllers: [],
   providers: [],
-  imports: [UserModule, SettingsModule, PrismaModule],
+  imports: [UserModule, SettingsModule, PrismaModule, ClientModule,],
 })
 export class AppModule {}
---
+++ api/src/modules/product-group/product-group.service.ts
@@ -0,0 +1,120 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductGroupDto } from './dto/create-product-group.dto';
+import { UpdateProductGroupDto } from './dto/update-product-group.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { HttpError } from 'src/common/exception/http.error';
+import { FindAllProductGroupQueryDto } from './dto/findAll-product-group.dto,';
+
+@Injectable()
+export class ProductGroupService {
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createProductGroupDto: CreateProductGroupDto) {
+    const creator = await this.prisma.user.findUnique({
+      where: {
+        id: createProductGroupDto.creatorId,
+      },
+    });
+    if (!creator) {
+      throw new HttpError({ code: 'Creator not found' });
+    }
+    const productGroup = await this.prisma.productGroup.create({
+      data: {
+        name: createProductGroupDto.name,
+        creatorId: createProductGroupDto.creatorId,
+        modifyId: createProductGroupDto.modifyId,
+      },
+    });
+    return productGroup;
+  }
+  async findAll(dto: FindAllProductGroupQueryDto) {
+    const { limit = 10, page = 1, name } = dto;
+
+    const [data, total] = await this.prisma.$transaction([
+      this.prisma.productGroup.findMany({
+        where: {
+          name: {
+            contains: name?.trim() || '',
+            mode: 'insensitive',
+          },
+        },
+        skip: (page - 1) * limit,
+        take: limit,
+        orderBy: { createdAt: 'desc' },
+        select: {
+          id: true,
+          name: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      }),
+      this.prisma.productGroup.count({
+        where: {
+          name: {
+            contains: name?.trim() || '',
+            mode: 'insensitive',
+          },
+        },
+      }),
+    ]);
+
+    return {
+      total,
+      page,
+      limit,
+      data,
+    };
+  }
+
+  async findOne(id: number) {
+    const productGroup = await this.prisma.productGroup.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!productGroup) {
+      throw new HttpError({ code: 'ProductGroup not found' });
+    }
+    return productGroup;
+  }
+
+  async update(
+    id: number,
+    updateProductGroupDto: UpdateProductGroupDto,
+    userId: number,
+  ) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw HttpError({ message: `User with ID ${userId} not found` });
+    }
+
+    const existingGroup = await this.prisma.productGroup.findUnique({
+      where: { id },
+    });
+
+    if (!existingGroup) {
+      throw HttpError({ message: `Product group with ID ${id} not found` });
+    }
+
+    return this.prisma.productGroup.update({
+      where: { id },
+      data: {
+        name: updateProductGroupDto.name ?? existingGroup.name,
+        modifyId: userId,
+        isDeleted: updateProductGroupDto.isDeleted ?? existingGroup.isDeleted,
+      },
+    });
+  }
+
+  async remove(id: number, modifierId: number) {
+    return this.prisma.productGroup.update({
+      where: { id },
+      data: {
+        isDeleted: true,
+      },
+    });
+  }
+  
+}
