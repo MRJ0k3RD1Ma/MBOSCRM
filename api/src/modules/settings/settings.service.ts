cab0a8de940bb10b35ad6da46c592fcf393e384f
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-07-26 15:36:52 +0500

    fixed settings update

--- api/src/modules/settings/settings.service.ts
+++ api/src/modules/settings/settings.service.ts
@@ -1,15 +1,16 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Setting } from '@prisma/client';
+import { UpdateSettingsDto } from './dto/update-settings.dto';
 
 @Injectable()
 export class SettingsService implements OnModuleInit {
   constructor(private readonly prisma: PrismaService) {}
 
-  onModuleInit() {
-    let settings = this.prisma.setting.findFirst({ where: { id: 1 } });
+  async onModuleInit() {
+    let settings = await this.prisma.setting.findFirst({ where: { id: 1 } });
+    console.log(settings);
     if (!settings) {
-      settings = this.prisma.setting.create({
+      settings = await this.prisma.setting.create({
         data: { id: 1, name: 'default' },
       });
     }
@@ -20,7 +21,7 @@ export class SettingsService implements OnModuleInit {
     return settings;
   }
 
-  update(data: Setting) {
+  update(data: UpdateSettingsDto) {
     return this.prisma.setting.update({ where: { id: 1 }, data: { ...data } });
   }
 }
