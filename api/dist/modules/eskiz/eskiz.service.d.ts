f5810d3bdad8676ed53a31e11c77901bed2789f7
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2025-09-13 17:45:14 +0500

    add

--- api/dist/modules/eskiz/eskiz.service.d.ts
+++ api/dist/modules/eskiz/eskiz.service.d.ts
@@ -6,8 +6,13 @@ export declare class EskizService implements OnModuleInit {
     private axios;
     constructor(prisma: PrismaService);
     onModuleInit(): Promise<void>;
+<<<<<<< Updated upstream
     callback(dto: EskizCallbackDto): Promise<boolean>;
     sendMessage(message: Awaited<ReturnType<typeof this.prisma.detailization.findFirst>>): Promise<any>;
     getTemplates(): Promise<any>;
+=======
+    sendMessage(mobile_phone: string, message: string, from?: string, callback_url?: string): Promise<any>;
+    getTemplates(): Promise<import("axios").AxiosResponse<any, any, {}>>;
+>>>>>>> Stashed changes
     private getToken;
 }
