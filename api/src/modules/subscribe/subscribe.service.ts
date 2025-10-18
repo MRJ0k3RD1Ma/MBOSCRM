159b3fff31ebd3118093bb44218046b6a8e87642
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-10-18 12:26:04 +0500

    fixed subscription price

--- api/src/modules/subscribe/subscribe.service.ts
+++ api/src/modules/subscribe/subscribe.service.ts
@@ -159,7 +159,7 @@ export class SubscribeService implements OnModuleInit {
 
 		const subscribe = await this.prisma.subscribe.create({
 			data: {
-				paid: 0,
+				paid: price,
 				paying_date: payingDate,
 				price,
 				state,
